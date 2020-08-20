5f5cef7c156c28d1f006ae48a4ae67c453427b47
DOMINGUEZ GARCIA <n381660@santanderglobaltech.com>
2020-08-20 14:56:52 +0200

    dependant assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependantAssertions() {
+        Owner owner = new Owner(1l, "Isra", "Dominguez");
+        owner.setCity("Rivas");
+        owner.setTelephone("737373");
+
+        assertAll("All Properties",
+                () -> assertEquals("Isra", owner.getFirstName()),
+                () -> assertEquals("Dominguez", owner.getLastName()),
+                () -> assertAll("Extra Properties",
+                        () -> assertEquals("Rivas", owner.getCity())
+                )
+        );
+    }
+
+}
