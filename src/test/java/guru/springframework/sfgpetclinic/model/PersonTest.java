bf99f343e5dee7d2a87d62b9953ce4152e382fa0
Israel Dominguez <israel.dominguezgarcia@santander.co.uk>
2020-09-28 17:44:19 +0200

    repeated test

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,7 +1,9 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,5 +20,11 @@ class PersonTest {
                 () -> assertEquals("Isra", person.getFirstName(), "first fail"),
                 () -> assertEquals("Dominguez", person.getLastName(), "second fail")
         );
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName}: {currentRepetition}")
+    @Test
+    void myRepeatedTest() {
+        System.out.println("repiter");
     }
 }
